6f88b34180e0e40678848a993f797b8bcf20fbe0
Ben Crowder <ben.crowder@gmail.com>
2011-08-19 10:49:49 -0600

    Added date picker

--- bookkeeper.js
+++ bookkeeper.js
@@ -203,6 +203,26 @@ function loadPage() {
 	});
 }
 
+$.extend(DateInput.DEFAULT_OPTS, {
+	stringToDate: function(string) {
+		var matches;
+		if (matches = string.match(/^(\d{4,4})-(\d{2,2})-(\d{2,2})$/)) {
+			return new Date(matches[1], matches[2] - 1, matches[3]);
+		} else {
+			return null;
+		};
+	}, 
+	dateToString: function(date) {
+		var month = (date.getMonth() + 1).toString();
+		var dom = date.getDate().toString();
+		if (month.length == 1) month = "0" + month;
+		if (dom.length == 1) dom = "0" + dom;
+		return date.getFullYear() + "-" + month + "-" + dom;
+	}
+});
+
 $(document).ready(function () {
 	loadPage();
+
+	$(".date_input").date_input();
 });
