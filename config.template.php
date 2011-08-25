86e1ffb594b76b54b107963f764a950bb5482588
chadgh <chadgh@gmail.com>
2011-08-25 16:27:18 -0600

    added APP_URL define to template config file

--- config.template.php
+++ config.template.php
@@ -1,6 +1,7 @@
 <?php
 define('APP_HOST', 'http://example.com');
 define('APP_PATH', '/full/path/to/root/of/application');
+define('APP_URL', '/bookkeeper');
 define('APP_VERSION', '1.0');
 
 define('DB_HOST', 'localhost');
