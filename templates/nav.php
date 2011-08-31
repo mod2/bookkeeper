ee3adf27825ad48968eb880ea8cd2c39d9d9f29b
chadgh <chadgh@gmail.com>
2011-08-31 14:37:02 -0700

    fixed nav.php for when there is not current_book variable set (like on the dashboard page or the add books)

--- templates/nav.php
+++ templates/nav.php
@@ -2,7 +2,7 @@
 	<h3>Current Books</h3>
 	<ul id="booklist">
 	<?php foreach ($args->books as $book): ?>
-		<li<?php if ($book->getBookId() == $args->current_book->getBookId()) { echo ' class="selected"'; }?>>
+		<li<?php if ($args->current_book && $book->getBookId() == $args->current_book->getBookId()) { echo ' class="selected"'; }?>>
 			<a id="book<?php echo $book->getBookId(); ?>" class="booklink" href="<?php echo $args->app_url . '/' . $args->username . '/book/' . $book->getSlug(); ?>"><?php echo $book->getTitle(); ?>
 				<div class="percentage"><div class="percentage_container"><div class="percent" style="width: <?php echo $book->getPercentageComplete(); ?>px;"></div></div>
 				<span><b><?php echo $book->getPercentageComplete(); ?>%</b> (<?php echo $book->getPagesLeft(); ?> pages left)</span></div>
