40cae106f3f9fa90fb830960cdc281d0378e43d8
chadgh <chadgh@gmail.com>
2011-09-01 16:07:07 -0700

    changed the account link to point to the right place

--- templates/subfooter.php
+++ templates/subfooter.php
@@ -1,4 +1,4 @@
 <footer>	
 	<span>Bookkeeper crafted lovingly by <a href="http://bencrowder.net/">Ben Crowder</a> and <a href="http://chadgh.com/">Chad Hansen</a>.</span>
-	<div id="subfooterlinks"><a href="<?php echo $args->app_url; ?>/account">Account</a> &bull; <a href="<?php echo $args->app_url; ?>/logout">Logout</a></div>
+	<div id="subfooterlinks"><a href="<?php echo $args->app_url . '/' . $args->username; ?>/account">Account</a> &bull; <a href="<?php echo $args->app_url; ?>/logout">Logout</a></div>
 </footer>
