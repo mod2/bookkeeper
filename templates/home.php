be2dc09a7394fae3a7a673ff1da19f26e484602e
chadgh <chadgh@gmail.com>
2011-09-01 17:33:03 -0700

    added some content to the home page

--- templates/home.php
+++ templates/home.php
@@ -1,4 +1,10 @@
 	<div id="view" >
-		<h1>Welcome</h1>
-
+		<h2>Dashboard</h2>
+		<ul>
+		<?php foreach ($args->books as $book): ?>
+			<?php if ($book->isTodayAReadingDay() && $book->getPagesToday() > 0): ?>
+			<li><?php echo $book->getTitle(); ?> - <?php echo Book::getPageString($book->getPagesToday()); ?></li>
+			<?php endif; ?>
+		<?php endforeach; ?>
+		</ul>
 	</div>
