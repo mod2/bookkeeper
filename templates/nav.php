5b9b0cf6b233bd75b14277ed774a1471f5bc4e3d
bencrowder <ben.crowder@gmail.com>
2011-09-02 10:18:13 -0600

    Updated nav.php

--- templates/nav.php
+++ templates/nav.php
@@ -3,11 +3,7 @@
 		<li class="pagelink<?php if ($args->page == "home") { echo ' selected'; } ?>"><a href="<?php echo $args->app_url; ?>/<?php if (property_exists($args, "username")): echo $args->username; endif; ?>/">Home</a></li>
 		<?php if ($args->page != "newaccount"): ?><li class="pagelink last<?php if ($args->page == "all") { echo ' selected'; } ?>"><a href="<?php echo $args->app_url; ?>/<?php echo $args->username; ?>/all">All Books</a></li><?php endif; ?>
 	<?php foreach ($args->books as $book): ?>
-		<li<?php if ($args->page == "book" && property_exists($args, 'current_book') && $book->getBookId() == $args->current_book->getBookId()) { echo ' class="selected"'; }?>>
-			<a id="book<?php echo $book->getBookId(); ?>" class="booklink" href="<?php echo $args->app_url . '/' . $args->username . '/book/' . $book->getSlug(); ?>"><?php echo $book->getTitle(); ?>
-				<div class="percentage"><div class="percentage_container"><div class="percent" style="width: <?php echo $book->getPercentageComplete(); ?>px;"></div></div>
-				<span><b><?php echo $book->getPercentageComplete(); ?>%</b> (<?php echo $book->getPagesLeft(); ?> pages left)</span></div>
-			</a>
+	<li<?php if ($args->page == "book" && property_exists($args, 'current_book') && $book->getBookId() == $args->current_book->getBookId()) { echo ' class="selected"'; }?>><a id="book<?php echo $book->getBookId(); ?>" class="booklink" href="<?php echo $args->app_url . '/' . $args->username . '/book/' . $book->getSlug(); ?>"><?php echo $book->getTitle(); ?> <div class="percentage"><div class="percentage_container"><div class="percent" style="width: <?php echo $book->getPercentageComplete(); ?>px;"></div></div><span><b><?php echo $book->getPercentageComplete(); ?>%</b> (<?php echo $book->getPagesLeft(); ?> pages left)</span></div></a>
 		</li>
 	<?php endforeach; ?>
 	</ul>
