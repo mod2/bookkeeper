264700e0011b60eec057c395052ab58075812db7
bencrowder <ben.crowder@gmail.com>
2011-09-02 09:58:23 -0600

    Removed DateInput formatter

--- js/bookkeeper.js
+++ js/bookkeeper.js
@@ -62,26 +62,6 @@ var Bookkeeper = function() {
 	};
 };
 
-$.extend(DateInput.DEFAULT_OPTS, {
-	stringToDate: function(string) {
-		var matches;
-		if (matches = string.match(/^(\d{4,4})-(\d{2,2})-(\d{2,2})$/)) {
-			return Date(matches[1], matches[2] - 1, matches[3]);
-		} else {
-			return null;
-		};
-	},
-	dateToString: function(date) {
-		dateObj = new Date(date);
-		fullyear = dateObj.getFullYear();
-		var month = (dateObj.getMonth() + 1).toString();
-		var dom = dateObj.getDate().toString();
-		if (month.length == 1) month = "0" + month;
-		if (dom.length == 1) dom = "0" + dom;
-		return fullyear + "-" + month + "-" + dom;
-	}
-});
-
 function parseDate(input) {
 	format = 'yyyy-mm-dd';
 
