8f44ad6be7b48d7228cf84e3ff5222088fd5008d
bencrowder <ben.crowder@gmail.com>
2011-10-08 10:53:37 -0600

    Fixed numpad bug

--- js/bookkeeper.js
+++ js/bookkeeper.js
@@ -166,9 +166,13 @@ $(document).ready(function() {
 
 	$("#currententry").keydown(function(e) {
 		var charCode = (e.which) ? e.which : e.keyCode;
-		if (charCode != 46 && charCode > 31 && (charCode < 48 || charCode > 57))
-			return false;
-		return true;
+
+		// allow backspace, home, end, arrows, insert, delete, 0-9, numpad 0-9, enter
+		if ((charCode == 8) || (charCode >= 35 && charCode <= 57) || (charCode >= 96 && charCode <= 105)) {
+			return true;
+		}
+
+		return false;
 	});
 
 	$("#currententry").change(function() {
